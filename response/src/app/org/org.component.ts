22654f5bb5df296d98668d8743f1221d0a9010c3
jeffj6123 <jeffrey.jarry@gmail.com>
2017-10-21 16:39:15 -0400

    a lot more logic for the org stuff

--- response/src/app/org/org.component.ts
+++ response/src/app/org/org.component.ts
@@ -1,12 +1,20 @@
 import {Component, OnInit} from '@angular/core';
 import {AngularFirestore, AngularFirestoreDocument, AngularFirestoreCollection} from "angularfire2/firestore";
 import {Observable} from "rxjs";
-import {AngularFireDatabase, AngularFireList} from "angularfire2/database";
+import {AngularFireDatabase, AngularFireList, AngularFireAction} from "angularfire2/database";
 import {AuthService, User} from "../auth.service";
 import {ActivatedRoute} from "@angular/router";
+import * as firebase from 'firebase/app';
 
 export interface Org {
+  name: string;
+  teams: Team[];
+  users: any;
+}
 
+export interface Team {
+  name: string;
+  premadePins: string[];
 }
 @Component({
   selector: 'app-org',
@@ -15,20 +23,30 @@ export interface Org {
 })
 export class OrgComponent implements OnInit {
 
+  id;
   orgRef: AngularFirestoreDocument<Org>;
   org: Observable<Org>;
   users: Observable<any[]>;
-
+  teams = <any>[];
   newMemberEmail = '';
+  newTeamName = '';
+  newPin = '';
+  addPinIndex = -1;
+  orgCopy;
 
-  constructor(public auth: AuthService, private afs: AngularFirestore, private route: ActivatedRoute) {
+  incidents;
+  incidentsRef;
+  newIncident = '';
+
+  constructor(public auth: AuthService, private afs: AngularFirestore, private route: ActivatedRoute, public db: AngularFireDatabase) {
   }
 
   ngOnInit() {
-    let id = this.route.snapshot.paramMap.get('id');
-    this.orgRef = this.afs.doc<Org>('org/' + id);
+    this.id = this.route.snapshot.paramMap.get('id');
+    this.orgRef = this.afs.doc<Org>('org/' + this.id);
     this.org = this.orgRef.valueChanges();
-    let usersQ = this.afs.collection('users', ref => ref.where('OrgIds.' + id,'==',true));
+    this.org.subscribe(res => { console.log(res); this.orgCopy = res; this.teams = res.teams});
+    let usersQ = this.afs.collection('users', ref => ref.where('OrgIds.' + this.id,'==',true));
     this.users = usersQ.snapshotChanges().map(actions => {
       return actions.map(a => {
         const data = a.payload.doc.data();
@@ -37,12 +55,32 @@ export class OrgComponent implements OnInit {
         return {id, data };
       });
     });
+
+    this.incidentsRef = this.db.list('incidents');
+    let incidents = this.db.list('incidents', ref => ref.orderByChild('orgId').equalTo(this.id));
+    this.incidents = incidents.valueChanges();
   }
 
 
   addMember(){
-    let user = this.afs.collection('users', ref => ref.where('uid','==',this.newMemberEmail).limit(1));
+    // let user = this.afs.collection('users', ref => ref.where('uid','==',this.newMemberEmail).limit(1));
     // user.update({OrgIds[this.org.uid] : true})
+  }
+
+  addTeam(name: string){
+    this.orgCopy.teams.push({name: name, premadePins: []});
+    this.orgRef.update(this.orgCopy).then( r => {this.newTeamName = '';});
+  }
+
+  addPin(text: string, index: number){
+    this.orgCopy.teams[index].premadePins.push(text);
+    this.orgRef.update(this.orgCopy)
+  }
+
+  addIncident() {
+    this.incidentsRef.push({name: this.newIncident, orgId: this.id}).then((item) => {
+      this.newIncident = '';
+    })
   }
 
 }
