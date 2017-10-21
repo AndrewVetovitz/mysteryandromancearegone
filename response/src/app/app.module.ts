dfc8f748a9026e6afd46cd5c78d1d1588767ddd7
Andrew Vetovitz <AndrewVetovitz@gmail.com>
2017-10-21 11:57:30 -0400

    small change

--- response/src/app/app.module.ts
+++ response/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { environment } from '../environments/environment';
-import {AgmCoreModule} from "@agm/core";
+import { AgmCoreModule } from "@agm/core";
 import { AppComponent } from './app.component';
 
 @NgModule({
