d7cc54d90897326702b4e1e8d95239a66251ee4d
jeffj6123 <jeffrey.jarry@gmail.com>
2017-10-21 23:15:50 -0400

    added more color features

--- response/src/app/incident/incident.component.ts
+++ response/src/app/incident/incident.component.ts
@@ -13,6 +13,11 @@ import { DrawingManager } from '@ngui/map';
   styleUrls: ['./incident.component.css']
 })
 export class IncidentComponent implements OnInit {
+
+  colors = {police:'#1E90FF',
+            firedept:'#b30000',
+            paramedics:'#999900'};
+
   // Name and start point of the map
   title: string = 'My first AGM project';
   lat: number = 51.678418;
@@ -29,13 +34,19 @@ export class IncidentComponent implements OnInit {
   org: Observable<Org>;
 
   polygonsHandler: Observable<any>;
-
+  circlesHandler: Observable<any>;
+  squaresHandler: Observable<any>;
   selectedOverlay: any;
   @ViewChild(DrawingManager) drawingManager: DrawingManager;
 
+  //DONT REALLY USE THESE NOT ASYNC SO NOT ALWAYS GOING TO GET THEM
+  userInfo;
+  orgInfo;
   constructor(private route: ActivatedRoute, public db: AngularFireDatabase, private afs: AngularFirestore, public auth: AuthService) { }
 
   ngOnInit() {
+    this.auth.user.subscribe(user => this.userInfo = user);
+
     this.id = this.route.snapshot.paramMap.get('id'); //Use for specific keys later
     this.itemsRef = this.db.list('incidents/' + this.id + '/markers');
     this.items = this.itemsRef.snapshotChanges().map(changes => {
@@ -45,24 +56,26 @@ export class IncidentComponent implements OnInit {
     this.incident = this.db.object('incidents/' + this.id );
     this.incident.valueChanges().subscribe(doc => {
       this.org = this.afs.doc('org/' + doc.orgId).valueChanges();
-
+      this.org.subscribe( res =>{ this.orgInfo = res;})
     });
 
+
+
     let polygons = this.db.list('incidents/' + this.id + '/polygons');
-    this.polygonsHandler = polygons.valueChanges()
-    //   .map(changes => {
-    //   return changes.map(c => (
-    //     {points: [[c.payload.val().points]]})
-    //   );
-    // });
+    this.polygonsHandler = polygons.valueChanges();
     this.polygonsHandler.subscribe(res =>
     {console.log(res)} );
 
+    let circles = this.db.list('incidents/' + this.id + '/circles');
+    this.circlesHandler = circles.valueChanges();
+
+    let squares = this.db.list('incidents/' + this.id + '/squares');
+    this.squaresHandler = squares.valueChanges();
+
     this.drawingManager['initialized$'].subscribe(dm => {
       google.maps.event.addListener(dm, 'overlaycomplete', event => {
-        if (event.type !== google.maps.drawing.OverlayType.MARKER) {
+        if (event.type == google.maps.drawing.OverlayType.POLYGON) {
           dm.setDrawingMode(null);
-          google.maps.event.addListener(event.overlay, 'click', e => {
             this.selectedOverlay = event.overlay;
             this.selectedOverlay.setEditable(true);
             console.log(event.overlay.getPath().getArray());
@@ -70,10 +83,33 @@ export class IncidentComponent implements OnInit {
             event.overlay.getPath().getArray().forEach(point => {
               points.push({lat :  point.lat(), lng : point.lng()})
             });
-            let newPoly = { points :points };
+
+            let newPoly = { points :points, color: this.colors[this.userInfo.OrgIds['nO2epGylHwK2A8CZK614']] };
             polygons.push(newPoly);
-          });
           this.selectedOverlay = event.overlay;
+        }else if(event.type == google.maps.drawing.OverlayType.CIRCLE){
+          dm.setDrawingMode(null);
+            this.selectedOverlay = event.overlay;
+            this.selectedOverlay.setEditable(true);
+            console.log(event);
+
+          let newCircle = {
+            center: {lat: event.overlay.center.lat(), lng: event.overlay.center.lng()},
+            radius: event.overlay.getRadius(),
+            color: this.colors[this.userInfo.OrgIds['nO2epGylHwK2A8CZK614']]
+          };
+            circles.push(newCircle);
+        }else if(event.type == google.maps.drawing.OverlayType.RECTANGLE){
+          dm.setDrawingMode(null);
+            this.selectedOverlay = event.overlay;
+            this.selectedOverlay.setEditable(true);
+            console.log(event.overlay.getPath().getArray());
+            let points = [];
+            event.overlay.getPath().getArray().forEach(point => {
+              points.push({lat :  point.lat(), lng : point.lng()})
+            });
+            let newPoly = { points :points, color: this.colors[this.userInfo.OrgIds['nO2epGylHwK2A8CZK614']] };
+            polygons.push(newPoly);
         }
       });
     });
